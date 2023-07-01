ada636461afca911e73ba5837fe2a6ccf5c0390f
unknown <veronika.dogareva@yandex.ru>
2023-07-01 21:48:17 +0300

    project 13.9

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => console.log('Подключено к MongoDB'))
-  .catch((err) => {
-    console.error('Ошибка подключения к MongoDB:', err);
-  });
+});
 app.use((req, res, next) => {
   req.user = {
     _id: '649b06fe957b6fc8b5d04b9c',
